4eb70d1a34554aca8f72496d8d9d087de3b126ed
Rebecca Benjamin <rdbindia@gmail.com>
2021-02-08 10:54:24 -0500

    https

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,7 +4,7 @@
         src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
         crossorigin="anonymous"></script>
-    <script src="https://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+    <script src="{{ url('assets/js/leaflet-heatmap/leaflet.js',[],true) }}"></script>
     <script src="{{ url('assets/js/heatmap.js',[],true) }}"></script>
     <script src="{{ url('assets/js/leaflet-heatmap/leaflet-heatmap.js',[],true) }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/0.4.2/leaflet.draw.js"></script>
