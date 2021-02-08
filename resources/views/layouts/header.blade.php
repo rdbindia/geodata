98faaa2b05c22f735e22d8a9e1eb3add5aa0bd29
Rebecca Benjamin <rdbindia@gmail.com>
2021-02-08 11:05:58 -0500

    duh!

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,7 +12,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 
         <link rel="stylesheet" href="{{ url('assets/css/heat_map.css',[],true) }}">
-        <link rel="stylesheet" href={{ url('assets/css/leaflet.css') }}/>
+        <link rel="stylesheet" href="{{ url('assets/css/leaflet.css',[],true) }}"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/0.4.2/leaflet.draw.css"/>
 
     @show
